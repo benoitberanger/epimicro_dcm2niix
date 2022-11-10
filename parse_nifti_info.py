e526149168a44af577f698c3cd983477e7e77df8
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-11-10 17:34:12 +0100

    DWI : unique & count of b-value

--- parse_nifti_info.py
+++ parse_nifti_info.py
@@ -6,6 +6,7 @@ import re       # regular expressions
 import logging  # to print
 import pandas   # for DataFrame
 import numpy as np
+import io       # ?
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -135,8 +136,110 @@ def clean_address(input):
         return re.sub('[^a-zA-Z0-9_]+', ' ', input)
     else:
         return input
-
 df['InstitutionAddress'] = df['InstitutionAddress'].apply(clean_address)
+
+
+# is DWI ?
+# df['ImageTypeStr'] = df['ImageType'].apply(lambda s: '_'.join(s))
+
+def read_bvals_bvecs(fbvals, fbvecs):
+    """Read b-values and b-vectors from disk.
+    Parameters
+    ----------
+    fbvals : str
+       Full path to file with b-values. None to not read bvals.
+    fbvecs : str
+       Full path of file with b-vectors. None to not read bvecs.
+    Returns
+    -------
+    bvals : array, (N,) or None
+    bvecs : array, (N, 3) or None
+    Notes
+    -----
+    Files can be either '.bvals'/'.bvecs' or '.txt' or '.npy' (containing
+    arrays stored with the appropriate values).
+    """
+    # Loop over the provided inputs, reading each one in turn and adding them
+    # to this list:
+    vals = []
+    for this_fname in [fbvals, fbvecs]:
+        # If the input was None or empty string, we don't read anything and
+        # move on:
+        if this_fname is None or not this_fname:
+            vals.append(None)
+            continue
+
+        if not isinstance(this_fname, str):
+            raise ValueError('String with full path to file is required')
+
+        base, ext = os.path.splitext(this_fname)
+        if ext in ['.bvals', '.bval', '.bvecs', '.bvec', '.txt',
+                   '.eddy_rotated_bvecs', '']:
+            with open(this_fname, 'r') as f:
+                content = f.read()
+
+            munged_content = io.StringIO(re.sub(r'(\t|,)', ' ', content))
+            vals.append(np.squeeze(np.loadtxt(munged_content)))
+        elif ext == '.npy':
+            vals.append(np.squeeze(np.load(this_fname)))
+        else:
+            e_s = "File type %s is not recognized" % ext
+            raise ValueError(e_s)
+
+    # Once out of the loop, unpack them:
+    bvals, bvecs = vals[0], vals[1]
+
+    # If bvecs is None, you can just return now w/o making more checks:
+    if bvecs is None:
+        return bvals, bvecs
+
+    if 3 not in bvecs.shape:
+        raise IOError('bvec file should have three rows')
+    if bvecs.ndim != 2:
+        bvecs = bvecs[None, ...]
+        bvals = bvals[None, ...]
+        msg = "Detected only 1 direction on your bvec file. For diffusion "
+        msg += "dataset, it is recommended to have at least 3 directions."
+        msg += "You may have problems during the reconstruction step."
+        logging.warning(msg)
+    if bvecs.shape[1] != 3:
+        bvecs = bvecs.T
+
+    # If bvals is None, you don't need to check that they have the same shape:
+    if bvals is None:
+        return bvals, bvecs
+
+    if len(bvals.shape) > 1:
+        raise IOError('bval file should have one row')
+
+    if bvals.shape[0] != bvecs.shape[0]:
+        raise IOError('b-values and b-vectors shapes do not correspond')
+
+    return bvals, bvecs
+
+df['isDWI'] = df['ImageType'].apply(lambda s: 'DIFFUSION' in s)
+
+bval_unique = []
+bval_count = []
+for row in df.index:
+    if df.loc[row,'isDWI']:
+        file_bval = os.path.splitext(df.loc[row, 'path'])[0] + '.bval'
+        file_bvec = os.path.splitext(df.loc[row, 'path'])[0] + '.bvec'
+        if os.path.exists(file_bval) and os.path.exists(file_bvec):
+            bval, bvec = read_bvals_bvecs(file_bval, file_bvec)
+            bval_rounded = [int(np.round(val/100)*100) for val in bval]
+            u, n = np.unique(bval_rounded, return_counts=True)
+            bval_unique.append(u)
+            bval_count.append(n)
+        else:
+            bval_unique.append(None)
+            bval_count.append(None)
+    else:
+        bval_unique.append(None)
+        bval_count.append(None)
+df['bval_unique'] = bval_unique
+df['bval_count'] = bval_count
+
 
 # THE END / write file
 logging.info('writing file')
